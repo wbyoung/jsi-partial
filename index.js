5dc29fde788d7bb853c3316e23c490998721e197
“Andreamsofcake” <andrea.moulding@gmail.com>
2014-09-17 16:03:51 -0700

    working on the partial project

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 'use strict';
 
 /**
- * Document me, please!
+ *
+ * 
+ *
  */
 module.exports.partial = function(fn) {
   var args = Array.prototype.slice.call(arguments);
@@ -11,4 +13,13 @@ module.exports.partial = function(fn) {
     partialArgs = partialArgs.concat(args2);
     return fn.apply({}, partialArgs);
   };
+};
+module.exports.partialRight = function(fn) {
+  var args = Array.prototype.slice.call(arguments);
+  var partialArgs = args.slice(1);
+  return function() {
+    var args2 = Array.prototype.slice.call(arguments);
+    partialArgs = args2.concat(partialArgs);
+    return fn.apply({}, partialArgs);
+  }
 };
