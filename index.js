a5d1f36511665588e6a11dbbef203a2faf9e1e58
Whitney Young <wbyoung@fadingred.com>
2014-09-17 14:26:56 -0700

    Partial function.

---
+++ index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * Document me, please!
+ */
+module.exports.partial = function(fn) {
+  var args = Array.prototype.slice.call(arguments);
+  var partialArgs = args.slice(1);
+  return function() {
+    var args2 = Array.prototype.slice.call(arguments);
+    partialArgs = partialArgs.concat(args2);
+    return fn.apply({}, partialArgs);
+  };
+};
--- test/tests.js
+++ test/tests.js
@@ -1,6 +1,35 @@
 var expect = require('chai').expect;
+var util = require('util');
 var lib = require('../index');
 
 describe('partial', function() {
-  it('has a pending test');
+  it('works for 2 arguments', function() {
+    var greet = function(greeting, name) {
+      return util.format('%s says %s', name, greeting);
+    };
+    var hi = lib.partial(greet, 'Hi');
+    var result = hi('Whitney');
+    expect(result).to.eql('Whitney says Hi');
+  });
+
+  it('works for 3 arguments', function() {
+    var greet = function(greeting, firstName, lastName) {
+      return util.format('%s %s says %s',
+        firstName, lastName, greeting);
+    };
+    var fullNameHi = lib.partial(greet, 'Hi');
+    var result = fullNameHi('Whitney', 'Young');
+    expect(result).to.eql('Whitney Young says Hi');
+  });
+
+  it('works for 3 arguments the other way', function() {
+    var greet = function(greeting, firstName, lastName) {
+      return util.format('%s %s says %s',
+        firstName, lastName, greeting);
+    };
+    var hiWhitney = lib.partial(greet, 'Hi', 'Whitney');
+    var result = hiWhitney('Nunn');
+    expect(result).to.eql('Whitney Nunn says Hi');
+  });
+
 });
